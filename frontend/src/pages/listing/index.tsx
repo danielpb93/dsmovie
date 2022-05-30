f151fbce2ff464b050e4cdeca10d2af3833d8723
danielpb93 <danielpb6293@gmail.com>
2022-05-30 01:54:37 -0300

    add moviecard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,5 +1,27 @@
+import { MovieCard } from "components/movieCard";
 import { Pagination } from "../../components/pagination";
 
 export function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
